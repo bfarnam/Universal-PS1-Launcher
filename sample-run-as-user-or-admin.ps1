9bdcb8172d1392034d4767ea882921af260a3445
bfarnam <brett_farnam@yahoo.com>
2025-01-07 16:55:47 -0600

    Corrected Spelling, Code, and inline documentation

--- sample-run-as-user-or-admin.ps1
+++ sample-run-as-user-or-admin.ps1
@@ -5,21 +5,20 @@ param(
 );
 
 <#
-    Copy and paste everything here to the very beginning of your script
-    If you pass parameters, you will have to modify the $newProcess.Arguments line
+    Copy the contents of this file to the very beginning of your scripts.
+    You can:
+        1.  Use this as a "template" and use the script blocks for your code
+        - OR -
+        2.  Replace the code block (BEGIN CODE to END CODE) with your code.
+    
+    NOTE:
+    If you pass parameters, you will have to modify the $newProcess.Arguments
+    line.
 #>
 
 # Do not modify the sections here - your code goes down at the bottom
 [bool]$bRunAsAdmin = $false;
-if ($RunAs -eq "Admin") { $bRunAsAdmin = $true; }
-
-$strScriptName = $PSCommandPath;
-$strScriptPath = $PSScriptRoot;
-$logfile = $strScriptName.Replace('.ps1','.log');
-$strVer = "v2.3";
-$strVerDate = "2025 January 5";
-$strAuthor = "Brett A. Farnam (brett_farnam@yahoo.com)";
-$strCopyNotice = "Copyright (c) 2025 "+$strAuthor;
+if ($RunAs -eq "Admin") { $bRunAsAdmin = $true; };
 
 <# ************************************************************************ #>
 <# ********************** BEGIN RUN AS ADMIN SECTION ********************** #>
@@ -57,7 +56,7 @@ else
         # Add -NoExit to prevent the screen from closing for troubleshooting
         # -WindowStyle Maximized makes sure that this window covers everything
         # -ExecutionPolicy Bypass is "just in case"
-        # We surround the $MyInvocation in 'single qoutes' in case of spaces in long pathnames
+        # We surround $MyInvocation.MyCommand.Definition in 'single quotes' in case of spaces in long path names
         $newProcess.Arguments = "-WindowStyle Maximized -ExecutionPolicy Bypass & '" + $myInvocation.MyCommand.Definition +"' -RunAs $RunAs";
         
         # Indicate that the process should be elevated
@@ -75,11 +74,21 @@ else
 <# *********************** END RUN AS ADMIN SECTION *********************** #>
 <# ************************************************************************ #>
 
+<# ****************************** BEGIN CODE ****************************** #>
 <#
-
-    This is an example of a script which loops through scriptblocks and gives a progress bar.
+    This is an example of a script which loops through script blocks and gives
+    a progress bar.
     Your code would replace this code.
 #>
+
+# Define the variables used
+$strScriptName = $PSCommandPath;
+$strScriptPath = $PSScriptRoot;
+$logfile = $strScriptName.Replace('.ps1','.log');
+$strVer = "v2.3";
+$strVerDate = "2025 January 5";
+$strAuthor = "Brett A. Farnam (brett_farnam@yahoo.com)";
+$strCopyNotice = "Copyright (c) 2025 "+$strAuthor;
 
 # Define the message to be displayed on the progress bar
 $message = 'Running scripts. Do not close this window.';
@@ -87,6 +96,7 @@ $message = 'Running scripts. Do not close this window.';
 # Define the actions to perform as script blocks
 $scripts = @(
     {
+        # Add an entry to a log file to show the BEGIN of the script
         $output = "**********************************************************************";
         $output += "`n"+(Get-Date -Format "yyyy-MM-dd HH:mm:ss")+" BEGIN "+$strScriptName;
         $output += "`n**********************************************************************";
@@ -96,6 +106,7 @@ $scripts = @(
         Start-Sleep -Seconds 1;
     };
     {
+        # Add an entry to a log file to show the information about the script and system environment
         Write-Output $strCopyNotice;
         $output = "Version "+$strVer+"    "+$strVerDate+"    "+$strAuthor;
         $output += "`n**********************************************************************";
@@ -121,19 +132,22 @@ $scripts = @(
         Start-Sleep -Seconds 1;
     };
     {
+        # Add an entry to a log file to show the END of the script
         $output = "`n**********************************************************************";
         $output += "`n"+(Get-Date -Format "yyyy-MM-dd HH:mm:ss")+" END "+$strScriptName;
         $output += "`nVersion "+$strVer+"    "+$strVerDate+"    "+$strAuthor;
         Write-Output $output;
         Write-Output $strCopyNotice;
         $output = "**********************************************************************";
+        # We add a couple of new lines at the very end to make it easier to find the BEGIN tag
+        # when there are multiple runs.
         $output += "`n`n";
         Write-Output $output;
         Start-Sleep -Seconds 1;
     };
 );
 
-# Construct the percentage bar and dispaly the message and progress bar while executing the script blocks
+# Construct the percentage bar and displays the message and progress bar while executing the script blocks
 & {
     [float] $complete = 0;
     [float] $increment = 100 / $scripts.Count;
@@ -143,3 +157,22 @@ $scripts = @(
         $complete += $increment;
     };
 } *>&1 >> $logfile;
+
+<#
+    NOTES
+        & { my code } 
+        purpose: executes all code within the code block
+        
+        & { my code } *>&1 >> $logfile;
+        purpose: executes all code with in the code block and redirects output
+        to the $logfile variable (Which is defined as the script name but ends in .log)
+        
+        *   : This is a wildcard character that matches all streams.
+        
+        >&1 : Redirects all streams (including standard output, error output,
+            and warning output) to the standard output stream (stream 1).
+            
+        >>  : Appends the output to an existing file, rather than overwriting it.
+              (This will create a file if it does not exist)
+#>
+<# ******************************* END CODE ******************************* #>
